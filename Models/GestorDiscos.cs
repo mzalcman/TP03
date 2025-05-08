a36d581a20ec6d2ff56a4685d00800e1671583c8
ELIEL DAN JAMILIS <49086719@ort>
2025-05-08 08:18:12 -0300

    Prueba Traer

--- Models/GestorDiscos.cs
+++ Models/GestorDiscos.cs
@@ -8,7 +8,7 @@ public class GestorDiscos
     {
         Discos = new Dictionary<int, Disco>();
         
-        
+    
     }
 
 
--- obj/Debug/net8.0/TРОЗ.AssemblyInfo.cs
+++ obj/Debug/net8.0/TРОЗ.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TРОЗ")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+476ef6ca00fb9d256a5721e314d4a0d9687788e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99159292efed4d68390e6d081ceb69dd484f7b80")]
 [assembly: System.Reflection.AssemblyProductAttribute("TРОЗ")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TРОЗ")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
