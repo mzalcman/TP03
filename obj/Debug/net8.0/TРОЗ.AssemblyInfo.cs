694158ce0c901af71d7a3a508782f451dfca9620
MORENA YAEL ZALCMAN <49089410@ort>
2025-05-09 14:33:45 -0300

    cambios

--- obj/Debug/net8.0/TРОЗ.AssemblyInfo.cs
+++ obj/Debug/net8.0/TРОЗ.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TРОЗ")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7ec453888fa6d4cdab3fcd88a40c671be0160ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac747f85e1ddbfe5bd52ba98b5f23f302da0a786")]
 [assembly: System.Reflection.AssemblyProductAttribute("TРОЗ")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TРОЗ")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
