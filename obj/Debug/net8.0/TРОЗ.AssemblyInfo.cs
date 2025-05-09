08f9c98c47c784c4337a0d422f89919b0384f9a6
ELIEL DAN JAMILIS <49086719@ort>
2025-05-09 15:12:21 -0300

    CSS+

--- obj/Debug/net8.0/TРОЗ.AssemblyInfo.cs
+++ obj/Debug/net8.0/TРОЗ.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TРОЗ")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e554244deabd0d8956ce0394f093562f3d5fa263")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+576ee90b3c399caa567783c19955feda4daf024a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TРОЗ")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TРОЗ")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
