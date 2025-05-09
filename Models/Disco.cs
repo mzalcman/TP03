50657e4b4b692ac519f9b71d2c0237753a4f9ac9
ELIEL DAN JAMILIS <49086719@ort>
2025-05-09 14:08:56 -0300

    Cargo informacion

--- Models/Disco.cs
+++ Models/Disco.cs
@@ -7,12 +7,12 @@ public class Disco
     public string artista { get; private set; }
     public string productor { get; private set; }
     public string genero { get; private set; }
-    public DateTime FechaLanzamiento { get; private set; }
+    public DateTime fechaLanzamiento { get; private set; }
     public List<Temas> temas { get; private set; }
     public int id { get; private set; }
     public string foto { get; private set; }
 
-    public Disco(string artista, string productor, string genero, List <Temas> temas, int id, string foto)
+    public Disco(string artista, string productor, string genero, List <Temas> temas, int id, string foto, DateTime fechaLanzamiento)
     {
         this.artista = artista; 
         this.productor = productor;
@@ -20,5 +20,6 @@ public class Disco
         this.temas = temas;
         this.id = id;
         this.foto = foto;
+        this.fechaLanzamiento = fechaLanzamiento;
     }
 }
